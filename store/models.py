3cb693b71bea5e6bbe0dd979fde14470ed91cfa9
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-12-10 12:50:07 -0300

    Carrinho do Usuário, Dados da página de check-out

--- store/models.py
+++ store/models.py
@@ -18,10 +18,10 @@ class Customer(models.Model):#Cliente
     
 
 
- # transformar um método de uma classe em uma propriedade,
+ # @ property - transformar um método de uma classe em uma propriedade,
  # permitindo que você acesse esse método como se fosse um atributo.
  # Isso significa que você pode chamar o método sem usar parênteses. 
-@property 
+
 class Product(models.Model):#Produto
     """
     O modelo do produto representa os produtos que temos em loja.
@@ -34,6 +34,7 @@ class Product(models.Model):#Produto
     def __str__(self):
         return self.name
     
+    @property 
     def imageURL(self):
         try:
             url = self.image.url
@@ -43,20 +44,41 @@ class Product(models.Model):#Produto
 
 
 class Order(models.Model):#Ordem(um pedido será o resumo do pedido dos itens. Carrinho)
+
     """
     O modelo de pedido representará uma transação realizada ou pendente.
     O modelo conterá informações como ID da transação, dados concluídos
     e status do pedido. Este modelo será filho ou modelo do cliente, mas pai dos itens do pedido.
     """
+
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True) # data do pedido
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
-
-    def __str__(self):
+    def __str__(self): # Self é uma istância de Order
         return str(self.id)
     
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems]) # item contexto func cart, e do template cart
+        return total
+
+
+    @property
+    def get_cart_total(self):
+
+        """
+            Definimos um relacionamento entre modelos(Order e OrderItem) 
+            através de um fk, O DJANGO automaticamente cria um ATRIBUTO
+            no modelo relacionado usando o nome do modelo em minúsculas seguido de "_set".
+        """        
+        orderitems = self.orderitem_set.all() # VAR(carrinho) = Order.OrdemItem.todos()
+        total = sum([item.get_total for item in orderitems]) # item é contexto iterado em cart.html. get_total é uma func de OrderItem
+        return total
+
 
 class OrderItem(models.Model):#  itens no carrinho
     """
@@ -68,6 +90,12 @@ class OrderItem(models.Model):#  itens no carrinho
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
 
 
 class ShippingAddress(models.Model):#Endereço para envio
--- store/views.py
+++ store/views.py
@@ -3,15 +3,34 @@ from .models import *
 
 
 def store(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
+
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html', context)
+
+	if request.user.is_authenticated:           # Se usuário estiver autenticado
+		customer = request.user.customer        # cliente = solicita usuário cliente
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)    # encontre ou crie e faça o pedido usando o método get_or_create().
+		items = order.orderitem_set.all()       # consultar os itens do carrinho com order.orderitem_set.all() 
+	else: # Se o usuário não estiver autenticado/logado, queremos retornar uma lista vazia chamada itens.
+		items = [] 
+		order = {'get_cart_total':0, 'get_cart_items':0}
+		
+	context = {'items':items, 'order': order}
+	return render(request, 'store/cart.html', context)
+
 
 def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Se o usuário não estiver autenticado/logado, queremos retornar uma lista vazia chamada itens.
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
 
+	context = {'items':items, 'order':order}
+	return render(request, 'store/checkout.html', context)
